a501c4161c54f6b27b313a60d937ac6f153aba8a
Jimmaliaros <jimmaliaros@gmail.com>
2026-04-30 22:42:22 +0300

    First classes added

--- app/src/main/java/com/example/auebnavigator/MainActivity.java
+++ app/src/main/java/com/example/auebnavigator/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.auebnavigator;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private GestureDetectorCompat gestureDetectorCompat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        gestureDetectorCompat = new GestureDetectorCompat(this, new MyGestureListener());
+
     }
+
+    private class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
+            Toast.makeText(MainActivity.this, "DoubleTap", Toast.LENGTH_SHORT).show();
+            return super.onDoubleTapEvent(e);
+        }
+
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+
 }
