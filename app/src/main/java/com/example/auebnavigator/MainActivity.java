f05ee89bcff224a066efec45a62f55280b05c180
Jimmaliaros <jimmaliaros@gmail.com>
2026-05-05 19:22:26 +0300

    push all new classes created

--- app/src/main/java/com/example/auebnavigator/MainActivity.java
+++ app/src/main/java/com/example/auebnavigator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.auebnavigator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
@@ -31,17 +32,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
+
+        private static final int SWIPE_THRESHOLD = 100;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+
         @Override
+
+
         public boolean onDoubleTapEvent(@NonNull MotionEvent e) {
-            Toast.makeText(MainActivity.this, "DoubleTap", Toast.LENGTH_SHORT).show();
-            return super.onDoubleTapEvent(e);
+
+            Intent intent = new Intent(MainActivity.this, SpeakingScreen.class);
+            startActivity(intent);
+            return true;
         }
+
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            boolean result = false;
+            try {
+                float diffY = e2.getY() - e1.getY();
+                float diffX = e2.getX() - e1.getX();
+
+                // Determine if swipe is Horizontal or Vertical
+                if (Math.abs(diffX) > Math.abs(diffY)) {
+                    // Horizontal swipe
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            Intent intent = new Intent(MainActivity.this, SpeakingScreen.class);
+                            //System.out.println("Swipe Right");
+                        } else {
+                            System.out.println("Swipe Left");
+                        }
+                        result = true;
+                    }
+                } else {
+                    // Vertical swipe
+                    if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            System.out.println("Swipe Down");
+                        } else {
+                            System.out.println("Swipe Up");
+                        }
+                        result = true;
+                    }
+                }
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+            return result;
+        }
+
+
+
 
 
     }
 
     public boolean onTouchEvent(MotionEvent event){
         gestureDetectorCompat.onTouchEvent(event);
+
+
         return super.onTouchEvent(event);
     }
 
