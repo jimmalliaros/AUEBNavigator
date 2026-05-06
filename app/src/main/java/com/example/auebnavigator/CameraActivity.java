26107ab5ca6f8153762311ad9a8f93c369492db0
Jimmaliaros <jimmaliaros@gmail.com>
2026-05-06 19:02:45 +0300

    added code in CameraActivity

--- app/src/main/java/com/example/auebnavigator/CameraActivity.java
+++ app/src/main/java/com/example/auebnavigator/CameraActivity.java
@@ -1,7 +1,15 @@
 package com.example.auebnavigator;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 
-public class CameraActivity {
+public class CameraActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+    }
 }
